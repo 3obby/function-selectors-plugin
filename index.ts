275a7e39d791006f5f1ecb6850ab2ebd905ac6eb
Cameron White <c@meron.dev>
2024-10-21 22:45:23 -0500

    add basic error handling to params

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-
 import {extendConfig} from "hardhat/config"
+import {HardhatPluginError} from "hardhat/plugins";
 import "@nomiclabs/hardhat-ethers"
 import 'hardhat/types/config';
 import './tasks/compile';
@@ -10,7 +10,6 @@ interface FunctionSelectorsUserConfigEntry {
     orderedByValue?: boolean
 
     outputPath?: string,
-    outputFilename?: string,
 
     pretty?: boolean
     runOnCompile?: boolean
@@ -26,7 +25,6 @@ export interface FunctionSelectorsConfigEntry {
     orderedByValue: boolean
 
     outputPath: string,
-    outputFilename: string,
 
     pretty: boolean
     runOnCompile: boolean
@@ -50,8 +48,7 @@ declare module "hardhat/types/config" {
 const DEFAULT_CONFIG : FunctionSelectorsConfigEntry = {
     separateContractSelectors: true,
     orderedByValue: false,
-    outputPath: ".",
-    outputFilename: "selectors.json",
+    outputPath: "./selectors.json",
     pretty: true,
     runOnCompile: true,
     includeParams: true,
@@ -62,7 +59,36 @@ const DEFAULT_CONFIG : FunctionSelectorsConfigEntry = {
 
 extendConfig((config, userConfig) => {
     config.functionSelectors = [userConfig.functionSelectors].flat().map((el) => {
+        // if any values not provided by user, go with defaults
         const conf = Object.assign({}, DEFAULT_CONFIG, el)
+
+        // check if only/except have an overlap
+        const overlap = conf.only.filter(value => conf.except.includes(value))
+        if (overlap.length>0) {
+            throw new HardhatPluginError(
+                "function-selectors-plugin",
+                `\`only\` & \`except\` elements cannot overlap: [${overlap}]`,
+            );
+        }
+
+        // check if any selectors not correct len
+        const incorrectLen = conf.skipSelectors.filter((sel) => sel.length !== 10)
+        if(incorrectLen.length > 0){
+            throw new HardhatPluginError(
+                "function-selectors-plugin",
+                `selectors in \`skipSelectors\` must be 10 characters: [${incorrectLen}]`,
+            );
+        }
+
+        // check if any selectors don't start with "0x"
+        const incorrectPrefix = conf.skipSelectors.filter((sel) => !sel.startsWith("0x"))
+        if(incorrectLen.length > 0){
+            throw new HardhatPluginError(
+                "function-selectors-plugin",
+                `selectors in \`skipSelectors\` must be start with a \`0x\`: [${incorrectPrefix}]`,
+            );
+        }
+
         return conf as FunctionSelectorsConfigEntry
     })
 })
